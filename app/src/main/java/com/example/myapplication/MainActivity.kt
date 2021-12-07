4b6d56fb94009ed1c86ebdc9279c5c3706c97339
dipanshudiwaker <56470089+dipanshudiwaker@users.noreply.github.com>
2021-12-07 16:52:41 +0530

    Ca3 Asssigment

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.example.myapplication
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.graphics.BitmapFactory
+import com.google.android.material.snackbar.Snackbar
+import java.io.ByteArrayOutputStream
+
+
+class MainActivity : AppCompatActivity() {
+      private var radioGroup: RadioGroup? = null
+    private lateinit var radioButton: RadioButton
+    lateinit var C1: CheckBox
+    lateinit var C2: CheckBox
+    lateinit var C3: CheckBox
+    lateinit var C4: CheckBox
+    private  val CAMERA_PERMISSION_CODE = 100
+    val REQUEST_CODE=1
+    private lateinit var  btnimage:ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnimage=findViewById(R.id.btnimage)
+        C1 = findViewById(R.id.c1)
+        C2 = findViewById(R.id.c2)
+        C3 = findViewById(R.id.c3)
+        C4 = findViewById(R.id.c4)
+
+        btnimage.setOnClickListener {
+            checkPermission(
+                Manifest.permission.CAMERA,
+                CAMERA_PERMISSION_CODE)
+        }
+
+        radioGroup = findViewById(R.id.radiogroup1)
+        val user=findViewById<EditText>(R.id.e1)
+        val email=findViewById<EditText>(R.id.e2)
+        val btnsubmit:Button=findViewById(R.id.btnsubmit)
+
+        btnsubmit.setOnClickListener {
+
+            val intSelectButton: Int = radioGroup!!.checkedRadioButtonId
+            radioButton = findViewById(intSelectButton)
+            val result = StringBuilder()
+           //result.append(radioButton)
+            if (C1.isChecked) {
+                result.append("\nPythone")
+
+            }
+            if (C2.isChecked) {
+                result.append("\nJava")
+
+            }
+            if (C3.isChecked) {
+                result.append("\n C++")
+
+            }
+            if(C4.isChecked){
+                result.append("\n C")
+            }
+            intent= Intent(this,MainActivity2::class.java)
+         //  intent.putExtra("image",R.id.btnimage)
+            intent.putExtra("Name",user.text.toString())
+            intent.putExtra("Email",email.text.toString())
+            intent.putExtra("Domain",radioButton.text)
+         intent.putExtra("Programming_Lannguage",result.toString())
+            startActivity(intent)
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            btnimage.setImageBitmap(data.extras?.get("data") as Bitmap)
+        }
+    }
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this@MainActivity, permission) == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+        } else {
+            val snack= Snackbar.make(it,"User Data is Transfered Sucessfully", Snackbar.LENGTH_LONG)
+            snack.show()
+
+            val cameraIntent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+             startActivityForResult(cameraIntent,REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Camera Permission Granted", Toast.LENGTH_SHORT).show()
+                val cameraIntent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent,REQUEST_CODE)
+            } else {
+                Toast.makeText(this@MainActivity, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+        }
+    }
+
+
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,88 @@
+package com.example.myapplication
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.make
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val imageView: ImageView = findViewById(R.id.im2)
+        val bundle: Bundle = intent.extras!!
+        val resId: Int = bundle.getInt("image")
+        imageView.setImageResource(resId)
+
+        val message1 = intent.getStringExtra("Name")
+        val messageTextView1: TextView = findViewById(R.id.tex1)
+        messageTextView1.text = message1
+
+        val message2 = intent.getStringExtra("Email")
+        val messageTextView2: TextView = findViewById(R.id.tex2)
+        messageTextView2.text = message2
+
+        val message3 = intent.getStringExtra("Domain")
+        val messageTextView3: TextView = findViewById(R.id.tex3)
+        messageTextView3.text = message3
+
+        val message4 = intent.getStringExtra("Programming_Lannguage")
+        val messageTextView4: TextView = findViewById(R.id.tex4)
+        messageTextView4.text = message4
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        return when (id) {
+            R.id.Setting -> {
+                Toast.makeText(this, "Under Process ", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.Exit -> {
+
+                    val builder = AlertDialog.Builder(this)
+                    builder.setTitle(R.string.dialogTitle)
+                    builder.setMessage(R.string.dialogMessage)
+                    builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+                    builder.setPositiveButton("Yes"){dialogInterface, which ->
+                        finishAffinity()
+                    }
+                    builder.setNeutralButton("Cancel"){dialogInterface , which ->
+                        Toast.makeText(applicationContext,"clicked cancel\n operation cancel",Toast.LENGTH_LONG).show()
+                    }
+                    builder.setNegativeButton("No"){dialogInterface, which ->
+                        Toast.makeText(applicationContext,"clicked No",Toast.LENGTH_LONG).show()
+                    }
+
+                    val alertDialog: AlertDialog = builder.create()
+                    alertDialog.setCancelable(false)
+                    alertDialog.show()
+
+               true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+}
+
+
